bd5eae5181368936f169a81160538f109bef1307
Gabriel Santos <gs2625020@gmail.com>
2025-03-29 13:35:34 -0300

    ETIX-013/feat: add logout via profile screen

--- mobile_tixup/lib/features/profile/profile_page.dart
+++ mobile_tixup/lib/features/profile/profile_page.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_tixup/features/auth/services/auth_service.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreen();
+}
+
+class _ProfileScreen extends State<ProfileScreen> {
+  final authService = AuthService();
+
+  void logout() async {
+    await authService.signOut();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Perfil")),
-      body: Center(
-        child: Text(
-          "Perfil",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-        ),
+      appBar: AppBar(
+        title: const Text("Profile"),
+        actions: [
+          IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
+        ],
       ),
     );
   }
